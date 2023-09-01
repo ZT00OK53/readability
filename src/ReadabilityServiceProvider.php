2535d03d49aa40537563b0500633d18f2583bfa1
Omkar Kurmi <omkar.kurmi@zehntech.com>
2023-09-01 10:07:00 +0530

    stability added

--- src/ReadabilityServiceProvider.php
+++ src/ReadabilityServiceProvider.php
@@ -2,12 +2,12 @@
 namespace Zehntech\Readability;
  
 use Illuminate\Support\ServiceProvider;
-use Masterminds\HTML5;
+
 class ReadabilityServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->app->register(HTML5::class);
+
     }
  
     public function register()
